815698ebdebe6f60a5a5ab123b0e09139bc9245e
Wilson Wong <arkeanwong@gmail.com>
2025-09-05 22:39:56 +1000

    Initial Commit

---
+++ Taskmanager/TaskmanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskmanagerApp.swift
+//  Taskmanager
+//
+//  Created by Wong Wilson on 5/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct TaskmanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
